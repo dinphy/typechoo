aae9c58eb5a0b516d0f879f68c0326e68976bded
zhangpeng96 <sjiaomail@gmail.com>
2021-09-25 10:51:57 +0800

    用镜像站替换gravatar链接

--- var/Typecho/Common.php
+++ var/Typecho/Common.php
@@ -831,7 +831,7 @@ EOF;
             if (defined('__TYPECHO_GRAVATAR_PREFIX__')) {
                 $url = __TYPECHO_GRAVATAR_PREFIX__;
             } else {
-                $url = $isSecure ? 'https://secure.gravatar.com' : 'http://www.gravatar.com';
+                $url = $isSecure ? 'https://gravatar.loli.net' : 'http://gravatar.loli.net';
                 $url .= '/avatar/';
             }
 
