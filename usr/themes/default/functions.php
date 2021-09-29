d9cc4b8bdd2993ba77236a3ecedefd189d24d872
zhangpeng96 <sjiaomail@gmail.com>
2021-09-29 09:33:21 +0800

    主题自定义字段模板，后台编辑选项卡样式修正

--- usr/themes/default/functions.php
+++ usr/themes/default/functions.php
@@ -29,16 +29,75 @@ function themeConfig($form)
     $form->addInput($sidebarBlock->multiMode());
 }
 
-/*
+
 function themeFields($layout)
 {
-    $logoUrl = new \Typecho\Widget\Helper\Form\Element\Text(
-        'logoUrl',
+
+/** 主题自定义字段各模板
+    $text = new \Typecho\Widget\Helper\Form\Element\Text(
+        'FieldText',
         null,
         null,
-        _t('站点LOGO地址'),
-        _t('在这里填入一个图片URL地址, 以在网站标题前加上一个LOGO')
+        _t('单行文本'),
+        _t('描述')
     );
-    $layout->addItem($logoUrl);
-}
+    $layout->addItem($text);
+
+    $radio = new \Typecho\Widget\Helper\Form\Element\Radio(
+        'FieldRadio',
+        [
+            0 => _t('选项一'),
+            1 => _t('选项二'),
+            2 => _t('选项三')
+        ],
+        0,
+        _t('单选框')
+    );
+    $layout->addItem($radio);
+
+    $checkbox = new \Typecho\Widget\Helper\Form\Element\Checkbox(
+        'FieldCheckbox',
+        [
+            'choose1'    => _t('香蕉'),
+            'choose2'    => _t('苹果'),
+            'choose3'    => _t('鸭梨')
+        ],
+        null,
+        _t('复选框')
+    );
+    // $layout->addItem($checkbox);
+    $layout->addItem($checkbox->multiMode());
+
+    $select = new \Typecho\Widget\Helper\Form\Element\Select(
+        'FieldSelect',
+        [
+            'show' => '显示',
+            'hide' => '不显示'
+        ],
+        'show',
+        _t('下拉选择'),
+        _t('是否在本篇文章底部显示版权声明')
+    );
+    $layout->addItem($select);
+
+    $password = new \Typecho\Widget\Helper\Form\Element\Password(
+        'FieldPassword',
+        null,
+        null,
+        _t('密码'),
+        _t('建议使用特殊字符与字母、数字的混编样式.')
+    );
+    // $password->input->setAttribute('class', 'w-60');
+    $layout->addItem($password);
+
+    $textarea = new \Typecho\Widget\Helper\Form\Element\Textarea(
+        'FieldTextarea',
+        null,
+        "L1\nL2\nL3",
+        _t('多行文字')
+    );
+    $layout->addItem($textarea);
 */
+
+}
+
